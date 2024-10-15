d416f87cf2f971160a9eafb01014c3cd4a16b32d
matteo langlois <langlois.matteo49@gmail.com>
2024-10-15 12:53:39 +0200

    Improve DTO

--- src/main/java/com/example/demo/models/DTO/BoxCreationDTO.java
+++ src/main/java/com/example/demo/models/DTO/BoxCreationDTO.java
@@ -3,7 +3,7 @@ package com.example.demo.models.DTO;
 import lombok.Data;
 
 @Data
-public class BoxDTO {
+public class BoxCreationDTO {
   private Integer id;
   private String nom;
   private Integer quantite;
---
+++ src/main/java/com/example/demo/models/DTO/UserConnectionDTO.java
@@ -0,0 +1,6 @@
+package com.example.demo.models.DTO;
+
+public class UserConnectionDTO {
+  private String email;
+  private String password;
+}
