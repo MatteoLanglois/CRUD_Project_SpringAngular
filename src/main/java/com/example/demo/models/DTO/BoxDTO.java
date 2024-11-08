760a12677b0d78b4592ad8cb041840115febd545
matteo langlois <langlois.matteo49@gmail.com>
2024-11-08 16:52:09 +0100

    Fix BoxDTO because of the merge

--- src/main/java/com/example/demo/models/DTO/BoxDTO.java
+++ src/main/java/com/example/demo/models/DTO/BoxDTO.java
@@ -1,32 +1,6 @@
-<<<<<<< HEAD:backend/src/main/java/com/example/demo/models/DTO/BoxDTO.java
 package com.example.demo.models.DTO;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Builder
-@Getter
-@Setter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode
-public class BoxDTO {
-  private Integer id;
-  private String nom;
-  private Integer quantite;
-  private String description;
-  private String point_geo;
-}
-=======
-package com.example.demo.models.DTO;
-
+import com.example.demo.models.Box;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -59,38 +33,32 @@ public class BoxDTO {
 
   /**
    * L'identifiant unique de la boîte.
-   *
    * Ce champ représente l'identifiant de la boîte dans le système. Il est utilisé pour référencer la boîte de manière unique.
    */
   private Integer id;
 
   /**
    * Le nom de la boîte.
-   *
    * Ce champ contient le nom descriptif de la boîte. Cela peut inclure des informations sur le type de boîte ou son usage.
    */
   private String nom;
 
   /**
    * La quantité disponible de la boîte.
-   *
    * Ce champ représente le nombre de boîtes disponibles dans le système. Il indique la quantité de stock restant pour cette boîte.
    */
   private Integer quantite;
 
   /**
    * La description de la boîte.
-   *
    * Ce champ contient des informations supplémentaires sur la boîte, comme sa taille, son utilisation ou toute autre caractéristique.
    */
   private String description;
 
   /**
    * Le point géographique de la boîte.
-   *
    * Ce champ contient des informations géographiques sous forme de coordonnées ou d'une autre notation pour indiquer l'emplacement
    * de la boîte, par exemple un GPS ou une adresse.
    */
   private String point_geo;
 }
->>>>>>> 99cb1f249adb954e5bd49abd8b4f4c3eb6b6f846:src/main/java/com/example/demo/models/DTO/BoxDTO.java
